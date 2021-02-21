fe3db0265eb633c0980a489bd656cafcbaf00b08
mikeremerscheid <mikeremerscheid@gmail.com>
2021-02-21 07:46:30 -0500

    Learning arrays in JS

--- js/main.js
+++ js/main.js
@@ -2,17 +2,29 @@ const btn = document.getElementById("go-button");
 
 function buttonClicked(){
     console.log("Button clicked.");
-    let customText = document.getElementsByClassName("my-input");
-    let textArea = document.getElementsByClassName("my-textarea");
-    let results = document.getElementById("text");
-    
-
-    console.log(customText);
-    results.innerHTML = "Hello, " + customText[0].value;
-    results.innerHTML += "Message: " + textArea[0].text;
+    btn.removeEventListener("click", buttonClicked);
+    document.getElementById("text").innerHTML = "Don't do it!";
 }
 
 
+var hobbies = ["pizza", "gaming", "reading", "programming", "music"];
 
+console.log("I no longer enjoy", hobbies.pop());
+
+hobbies.push("archery");
+console.log("I no longer like", hobbies.shift());
+hobbies.unshift("blacksmithing");
+
+hobbies.forEach(function(item, index){
+    console.log("I like", item, index);
+});
+
+if (hobbies.indexOf("reading")>1){
+    console.log("I like reading!");
+}
+
+if (hobbies.indexOf("sports") === -1){
+    console.log("not in array.");
+}
 
 btn.addEventListener("click", buttonClicked);
